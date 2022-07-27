528a1f633548aeb689a15dc976574731eabf85ca
nasekajlo <72610049+nasekajlo@users.noreply.github.com>
2022-07-28 00:37:03 +0300

    Add files via upload

---
+++ lab 7/scripts/u_dw_ext_references/run.sql
@@ -0,0 +1,8 @@
+begin
+EXEC pkg_load_ext_ref_calendar.load_cls_days;
+EXEC pkg_load_ext_ref_calendar.load_cls_weeks;
+EXEC pkg_load_ext_ref_calendar.load_cls_months;
+EXEC pkg_load_ext_ref_calendar.load_cls_quarters;
+EXEC pkg_load_ext_ref_calendar.load_cls_years;
+EXEC pkg_load_ext_ref_calendar.load_ref_calendar;
+end;
---
+++ lab 7/scripts/u_dw_references/t_days/sq_days.sql
@@ -0,0 +1,6 @@
+--drop sequence u_dw_references.sq_day_id;
+alter session set current_schema = u_dw_references;
+
+create sequence u_dw_references.sq_day_id start with 1;
+
+grant SELECT on u_dw_references.sq_day_id to u_dw_ext_references;
